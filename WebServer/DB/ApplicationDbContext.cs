db98dc0b5d5ad54a8883632288c8f55828852dae
AndriiPala <andrijpalamariuk@gmail.com>
2025-03-19 13:50:19 +0200

    H-10 Add DbContext
    - Implement ApplicationDbContext
    - Add User navigative field
    - Format code

--- WebServer/DB/ApplicationDbContext.cs
+++ WebServer/DB/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using HamsterCoin.Domain;
 
 namespace HamsterCoin.Database
 {
@@ -7,10 +8,37 @@ namespace HamsterCoin.Database
         public required DbSet<User> Users { get; set; }
         public required DbSet<UserDetails> UserDetails { get; set; }
         public required DbSet<Game> Games { get; set; }
-        public required DbSet<DepHistory> DepHistory { get; set; }
-        public required DbSet<DepWithdraw> DepWithdraw { get; set; }
+        public required DbSet<DepositHistory> DepHistory { get; set; }
+        public required DbSet<WithdrawHistory> WithdrawHistory { get; set; }
         public required DbSet<Card> Cards { get; set; }
 
         //Maybe implement User_Cards
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserDetails>()
+                .HasOne(ud => ud.User)
+                .WithMany() //оскільки в User немає властивості UserDetails, ми просто вказуємо .WithMany() без параметрів
+                .HasForeignKey(ud => ud.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<DepositHistory>()
+                .HasOne(d => d.User)
+                .WithMany()
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<WithdrawHistory>()
+                .HasOne(w => w.User)
+                .WithMany()
+                .HasForeignKey(w => w.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+
+        //TODO
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseExceptionProcessor();
+        }
     }
 }
--- WebServer/DB/Domain/DepHistory.cs
+++ WebServer/DB/Domain/DepHistory.cs
@@ -2,26 +2,31 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace HamsterCoin.Domain{
-
-[Table("dep_history")]
-public class Deposit
+namespace HamsterCoin.Domain
 {
-    [Key]
-    [Column("id")]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
 
-    [Column("user_id")]
-    public long? UserId { get; set; }
+    [Table("dep_history")]
+    public class DepositHistory
+    {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
-    [Required]
-    [Column("sum_dep")]
-    public decimal SumDep { get; set; }
+        [ForeignKey(nameof(User))]
+        [Column("user_id")]
+        public long UserId { get; set; }
 
-    [Required]
-    [Column("date_dep")]
-    public DateTime DateDep { get; set; }
-}
+        [NotMapped]
+        public User? User { get; set; }
+
+        [Required]
+        [Column("sum_dep")]
+        public decimal SumDep { get; set; }
+
+        [Required]
+        [Column("date_dep")]
+        public DateTime DateDep { get; set; }
+    }
 
 }
