679104e3488476455a2c897922ef10947f9ac85b
AndriiPala <andrijpalamariuk@gmail.com>
2025-03-23 10:15:17 +0200

    H-23 Сonfigured the file .gitignore

---
+++ WebServer/Domain/User.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace HamsterCoin.Domain
+{
+
+    [Table("users")]
+    public class User
+    {
+
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int Id { get; set; }
+
+        [Required]
+        [Column("mail")] // Переглянути можливість [EmailAddres]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Mail { get; set; }
+
+        [Required]
+        [Column("password")]
+        [MaxLength(255)]
+        public string Password { get; set; }
+
+    }
+}
